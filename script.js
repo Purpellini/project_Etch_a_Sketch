867ac0ea154f4fee935f8d7af2ded94a55385769
Purpellini <purpel@PC.>
2025-03-20 15:58:04 +0100

    1.0

---
+++ script.js
@@ -0,0 +1,30 @@
+const container = document.querySelector(".container")
+
+const btn = document.querySelector("#btn")
+btn.addEventListener("click",() =>{
+    let number = parseInt(prompt("podaj ile w rzędzie"))
+    totalNumber = number*number
+    for(let i=0; i<totalNumber;i++){
+        const div = document.createElement("div")
+        div.classList.add("box")
+
+        div.dataset.redValue=255
+        
+        div.addEventListener("mouseenter", () => {
+            let redValue = parseInt(div.dataset.redValue)
+            redValue= redValue>0? redValue-25:0;
+            
+            div.dataset.redValue=redValue
+            div.style.backgroundColor = `rgb(${redValue},0,0)`
+        })
+        container.appendChild(div)
+        div.setAttribute("style", `width:${100/number}%; height: ${container.clientWidth/number}px`)
+    }
+
+})
+
+
+
+
+
+
